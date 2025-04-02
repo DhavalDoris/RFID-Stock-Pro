e0bf2b931a8006c53bcd0a7163613acab1671b2e
Dhaval <dhavaldoris@gmail.com>
2025-04-02 17:53:29 +0530

    hide key in cpp

--- app/src/main/jni/keys.c
+++
@@ -1,7 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jstring JNICALL
-Java_com_example_rfidstockpro_ui_activities_getKey(JNIEnv *env, jobject instance) {
-
-    return (*env)-> NewStringUTF(env, "TmF0aXZlNWVjcmV0UEBzc3cwcmQx");
-}
