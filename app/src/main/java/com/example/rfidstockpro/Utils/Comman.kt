5cc46414b345f147822b214ad3c90ed0d1399f0f
HP_PC <dhavaldoris@gmail.com>
2025-04-29 17:34:32 +0530

    - location permission check in android 14 - uploading execl ui - error handling

--- app/src/main/java/com/example/rfidstockpro/Utils/Comman.kt
+++ app/src/main/java/com/example/rfidstockpro/Utils/Comman.kt
@@ -2,6 +2,11 @@ package com.example.rfidstockpro.Utils
 
 import android.content.Context
 import android.net.Uri
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rfidstockpro.R
 import com.example.rfidstockpro.aws.AwsManager.getFileExtension
 import java.util.UUID
 
@@ -15,5 +20,37 @@ object Comman {
         return "media_${uuid}_$timestamp.$extension"  // Create unique filename
     }
 
+    public fun getFileName(uri: Uri,context: Context): String {
+        var name = ""
+        context.contentResolver.query(uri, null, null, null, null)?.use { cursor ->
+            val nameIndex = cursor.getColumnIndex(android.provider.OpenableColumns.DISPLAY_NAME)
+            if (cursor.moveToFirst() && nameIndex >= 0) {
+                name = cursor.getString(nameIndex)
+            }
+        }
+        return name
+    }
+    fun showCustomSnackbarBelowToolbar(activity: AppCompatActivity, anchor: View) {
+        val inflater = activity.layoutInflater
+        val customView = inflater.inflate(R.layout.custom_snackbar, null)
+
+        val parent = activity.findViewById<ViewGroup>(android.R.id.content)
+        val container = FrameLayout(activity)
+        container.addView(customView)
+
+        // Add just below the toolbar
+        val layoutParams = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.topMargin = anchor.bottom // place below toolbar
+
+        parent.addView(container, layoutParams)
+
+        // Auto-dismiss after 2.5 seconds
+        customView.postDelayed({
+            parent.removeView(container)
+        }, 2500)
+    }
 
 }
